5b2898a2586bf55ed5bcbe31b8c6009be37944fd
Jessica Sarlin <jessica.sarlin@gmail.com>
2020-02-03 19:08:56 +0200

    Tests for search method added

--- viikko1/tehtavat14-16/NhlStatistics1/src/test/java/ohtuesimerkki/StatisticsTest.java
+++ viikko1/tehtavat14-16/NhlStatistics1/src/test/java/ohtuesimerkki/StatisticsTest.java
@@ -18,13 +18,11 @@ public class StatisticsTest {
     Reader readerStub = new Reader() {
         public List<Player> getPlayers() {
             ArrayList<Player> players = new ArrayList<>();
-
             players.add(new Player("Semenko", "EDM", 4, 12));
             players.add(new Player("Lemieux", "PIT", 45, 54));
             players.add(new Player("Kurri",   "EDM", 37, 53));
             players.add(new Player("Yzerman", "DET", 42, 56));
             players.add(new Player("Gretzky", "EDM", 35, 89));
-
             return players;
         }
     };
@@ -38,8 +36,17 @@ public class StatisticsTest {
     }
 
     @Test
-    public void testi() {
-        assertEquals(3,3);
+    public void pelaajienMaalimaaratOikein() {
+        assertEquals(4, stats.search("Semenko").getGoals());
+        assertEquals(45, stats.search("Lemieux").getGoals());
+        assertEquals(37, stats.search("Kurri").getGoals());
+        assertEquals(42, stats.search("Yzerman").getGoals());
+        assertEquals(35, stats.search("Gretzky").getGoals());
+    }
+
+    @Test
+    public void olematontaPelaajaaEiLoydy() {
+        assertEquals(null, stats.search("Jessi"));
     }
 
 }
