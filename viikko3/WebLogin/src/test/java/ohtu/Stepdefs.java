545a96837d90d6c99e7ef03193ddc0034ed33bb7
Jessica Sarlin <jessica.sarlin@gmail.com>
2020-03-22 15:25:59 +0200

    Week 3: 9 done

--- viikko3/WebLogin/src/test/java/ohtu/Stepdefs.java
+++ viikko3/WebLogin/src/test/java/ohtu/Stepdefs.java
@@ -36,22 +36,17 @@ public class Stepdefs {
     @When("correct username {string} and incorrect password {string} are given")
     public void correctUsernameAndIncorrectPasswordAreGiven(String username, String password) {
         logInWith(username, password);
-    }    
+    }
     
+    @When("incorrect username {string} and incorrect password {string} are given")
+    public void incorrectUsernameAndIncorrectPasswordAreGiven(String username, String password) {
+        logInWith(username, password);
+    }
+
     @Then("user is not logged in and error message is given")
     public void userIsNotLoggedInAndErrorMessageIsGiven() {
         pageHasContent("invalid username or password");
         pageHasContent("Give your credentials to login");
-    }    
-    
-    @When("username {string} and password {string} are given")
-    public void usernameAndPasswordAreGiven(String username, String password) throws Throwable {
-        logInWith(username, password);
-    }   
-    
-    @Then("system will respond {string}")
-    public void systemWillRespond(String pageContent) throws Throwable {
-        assertTrue(driver.getPageSource().contains(pageContent));
     }
     
     @After
