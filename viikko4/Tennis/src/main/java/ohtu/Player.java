0f359a7e94f8a628090474375a27d9dbb457e89e
Jessica Sarlin <jessica.sarlin@gmail.com>
2020-04-11 14:56:07 +0300

    Tennis ready

---
+++ viikko4/Tennis/src/main/java/ohtu/Player.java
@@ -0,0 +1,33 @@
+package ohtu;
+
+public class Player {
+
+    private String name;
+    private int score;
+
+    public Player(String name) {
+        this.name = name;
+        this.score = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public void addPoint() {
+        this.score++;
+    }
+
+}
--- viikko4/Tennis/src/main/java/ohtu/TennisGame.java
+++ viikko4/Tennis/src/main/java/ohtu/TennisGame.java
@@ -1,78 +1,67 @@
 package ohtu;
 
+import java.util.HashMap;
+
 public class TennisGame {
-    
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+
+    private Player playerOne;
+    private Player playerTwo;
+
+    private HashMap<Integer, String> scoreStringMap;
 
     public TennisGame(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
+        this.playerOne = new Player(player1Name);
+        this.playerTwo = new Player(player2Name);
+        this.scoreStringMap = new HashMap<>();
+        scoreStringMap.put(0, "Love");
+        scoreStringMap.put(1, "Fifteen");
+        scoreStringMap.put(2, "Thirty");
+        scoreStringMap.put(3, "Forty");
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
+        if (playerName.equals(playerOne.getName()))
+            playerOne.addPoint();
         else
-            m_score2 += 1;
+            playerTwo.addPoint();
+    }
+
+    public String getTieScore(int score) {
+        StringBuilder sb = new StringBuilder();
+
+        if (score == 0) sb.append(scoreStringMap.get(0));
+        else if (score == 1) sb.append(scoreStringMap.get(1));
+        else if (score == 2) sb.append(scoreStringMap.get(2));
+        else if (score == 3) sb.append(scoreStringMap.get(3));
+
+        if (sb.toString().isEmpty()) sb.append("Deuce");
+        else sb.append("-All");
+        return sb.toString();
     }
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                case 3:
-                        score = "Forty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
-        }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
+        int tempScore;
+
+        if (playerOne.getScore() == playerTwo.getScore()) {
+            score = getTieScore(playerOne.getScore());
+
+        } else if (playerOne.getScore() >= 4 || playerTwo.getScore() >= 4) {
+            int minusResult = playerOne.getScore() - playerTwo.getScore();
+            if (minusResult == 1) score = "Advantage player1";
+            else if (minusResult == -1) score = "Advantage player2";
+            else if (minusResult >= 2) score = "Win for player1";
+            else score = "Win for player2";
+        } else {
+            int i = 1;
+            while (i < 3) {
+                if (i == 1) tempScore = playerOne.getScore();
+                else {
+                    score += "-";
+                    tempScore = playerTwo.getScore();
                 }
+                score += scoreStringMap.get(tempScore);
+                i++;
             }
         }
         return score;
